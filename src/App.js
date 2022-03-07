ffd4adb20d89dbc61b6a13609d11dd1c3f9e71f1
Moshe <moisherosenzweig@gmail.com>
2022-03-07 16:55:21 -0500

    ComponentsOrdered

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React, {useEffect, useState}from 'react';
-import Navbar from './Navbar';
+import React from 'react';
+import PriceFetchManager from './PriceFetchManager';
+// import Navbar from './Navbar';
 import DreamWallet from './DreamWallet';
-import WelcomeVibes from './WelcomeVibes';
+
 import About from './About';
-import ActualWallet from './ActualWallet';
+// import ActualWallet from './ActualWallet';
 
 
 
 function App() {
 // fetch data here and pass to 
-const [data, setData] = useState([])
 
-useEffect(()=> {
-fetch('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum&vs_currencies=usd&include_market_cap=true&include_24hr_change=true')
-.then(resp => resp.json())
-.then(data => {
-  setData(data)
-})
-
-},[])
-
-console.log(data);
 
 
 // filter data to get btc and eth prices
@@ -29,9 +19,7 @@ console.log(data);
 // btc={btc} eth={eth}
   return (
     <div>
-      <WelcomeVibes />
-      <Navbar/>
-      <ActualWallet />
+      <PriceFetchManager/>
       <DreamWallet />
       <About />
     </div>
--- src/Navbar.js
+++ src/Navbar.js
@@ -5,20 +5,20 @@ function Navbar() {
     <div>      
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
     <div className="container-fluid">
-      <a className="navbar-brand" href="">Crypto'Naire</a>
+      <a className="navbar-brand" href="">Cryptonaire</a>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarNavDropdown">
         <ul className="navbar-nav">
           <li className="nav-item">
-            <a className="nav-link active" aria-current="page" href="#">Home</a>
+            <a className="nav-link active" href="#">Actual Wallet</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#">Features</a>
+            <a className="nav-link" href="#">Dream Portfolio</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#">Pricing</a>
+            <a className="nav-link" href="#">About Us</a>
           </li>
           <li className="nav-item dropdown">
             <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
