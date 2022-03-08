83d21f08fdec08149b9dd3ad46df9a4367429f5e
Moshe <moisherosenzweig@gmail.com>
2022-03-08 15:14:53 -0500

    Dream__Bigger

--- src/WelcomeVibes.js
+++ src/WelcomeVibes.js
@@ -5,8 +5,20 @@ function WelcomeVibes({data}) {
   
   return (
     <div id='container'>
-     <h1 className="CenterPiece">Cryptonaire</h1>
-     <h5 className="CenterPiece">Dream Big</h5>
+     <h1 className="Cryptonaire">Cryptonaire</h1>
+     <h5 className="dreamBig">Dream Bigger</h5>
+
+     <div className='leftTitle'>
+        <h3>REIMAGINE YOUR CRYPTO</h3>
+        <br></br>
+        <h3 id='dreamBigger'> DREAM BIGGER </h3>
+     </div>
+
+     <div className='rightTitle'>
+       <img></img>
+       </div>
+
+
 
     </div>
   )
