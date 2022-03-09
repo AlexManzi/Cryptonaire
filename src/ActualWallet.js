1f462b5169f021fa25ebb6a0d4dae3cdc07da0d1
Alex Manzi <alex.manzi22@gmail.com>
2022-03-09 10:36:53 -0500

    text and touches

--- src/ActualWallet.js
+++ src/ActualWallet.js
@@ -8,18 +8,21 @@ function ActualWallet({data, handleBitcoinCalculation, userBitcoinTotal, handleE
   return (
 
     <div id="actualWallet">
-      <h2 id="walletHeading">Your Actual Wallet</h2>
-      <h3 id="walletHeading">Here is a place for you to visualize all the amount of crypto that you have in your wallet </h3>
-      <label id="actualWalletLabel">Enter the Amount of Bitcoin in your Wallet Here!</label>
-      <input type="text" id="userWalletInput" name="fname" placeholder="Your BTC" onChange={handleBitcoinCalculation}></input>
-      <label id="actualWalletLabel">Enter the Amount of Ethereum in your Wallet Here!</label>
-      <input type="text" id="userWalletInput" name="lname" placeholder="Your ETH" onChange={handleEthereumCalculation}></input>
+      <div id='inputs'>
+        <h2 id="walletHeading">Reality Wallet</h2>
+        <h3 id="liveCryptoHoldings"> Live Crypto Holdings  </h3>
+      
+
+        <label id="actualWalletLabel">Your BTC </label>
+        <input type="text" id="userWalletInput" name="fname" placeholder="Enter BTC" onChange={handleBitcoinCalculation}></input>
+        <label id="actualWalletLabel">Your ETH </label>
+        <input type="text" id="userWalletInput" name="lname" placeholder="Enter ETH" onChange={handleEthereumCalculation}></input>
+      </div>
       <div id="dollarValueChart">
         <h3>Dollar Value</h3>
-        <p>Your Current Amount of Bitcoin in Dollars is ${userBitcoinTotal}</p>
-        <p>Your Current Amount of Ethereum in Dollars is ${userEthereumTotal}</p>
-        <h4>The total value of your Defi Wallet is ${totalWallet}</h4>
-        <img src="https://blog.crypto.com/content/images/size/w1600/2022/01/220112-Header-Banner.png" id="globePic" alt="bgpic"/>
+        <p> Your Current BTC Holdings ${userBitcoinTotal}</p>
+        <p> Your Current ETH Holdings ${userEthereumTotal}</p>
+        <h4> Current Wallet Total = ${totalWallet}</h4>
       </div>
     </div>
   )
