86383cdaa5f3b7140faf756f44015d1c9b66f83f
Moshe <moisherosenzweig@gmail.com>
2022-03-09 15:28:55 -0500

    Card Begginings

--- src/CryptoCard.js
+++ src/CryptoCard.js
@@ -2,7 +2,17 @@ import React from 'react'
 
 function CryptoCard() {
   return (
-    <div></div>
+    <>
+ <div id="CryptoCard" className="card">
+  <div className="card-body">
+    <h5 className="card-title">Name</h5>
+    <br></br>
+    <p>Something BTC =</p>
+    <p> Someting ETH =</p>
+    <a id='deletebttn' href="#" className="btn btn-danger btn-sm rounded-0">X</a>
+  </div>
+</div>
+    </>
   )
 }
 
