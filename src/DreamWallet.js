6c34f14c47a5b3c5500151a2cb72ce1ea4aa0267
Alex Manzi <alex.manzi22@gmail.com>
2022-03-10 14:18:09 -0500

    Thurs After Lunch

--- src/DreamWallet.js
+++ src/DreamWallet.js
@@ -4,25 +4,27 @@ import CryptoCard from './CryptoCard'
 
 // 
 
-function DreamWallet({cardArray}) {
+function DreamWallet({}) {
   const [DreamBTCAmount, setDreamBTCAmount] = useState(null);
   const [BTCPrice, setBTCPrice] = useState(null)
   const [DreamEthAmount, setDreamEthAmount] = useState (null);
   const [EthPrice, setEthPrice] = useState(null);
   let [arrayName, setArrayName] = useState(null)
-  let [showArray, setShowArray] = useState([])
-  let [newArray, setNewArray] = useState([])
   let BTCTotal = DreamBTCAmount * BTCPrice;
   let EthTotal = DreamEthAmount * EthPrice;
   let TotalValue = EthTotal + BTCTotal
 
+  let [cardArray, setCardArray] = useState([])
+
+
   useEffect(() => {
     fetch('http://localhost:8001/cryptoCard')
     .then(resp => resp.json())
-    .then(data => {
-      setNewArray(newArray)
+    .then(card => {
+      setCardArray(card)
     })
-}, [])
+  }, [])
+
 
   function handleAddToJson() {
     console.log("hello")
@@ -34,7 +36,6 @@ function DreamWallet({cardArray}) {
       BTCTotal: BTCTotal,
       name: arrayName
     }
-    console.log(newObj)
     fetch('http://localhost:8001/cryptoCard', {
       method: "POST",
       headers: {
@@ -44,11 +45,13 @@ function DreamWallet({cardArray}) {
   })
     .then(resp => resp.json())
     .then(data=>{
-      setShowArray([...showArray, data]);
+      setCardArray([...cardArray, data]);
     })
     }
 
-    let mappedArray = showArray.map(card => {
+    
+
+    let mappedArray = cardArray.map(card => {
       return (
         <CryptoCard 
         key={card.id}
@@ -65,11 +68,11 @@ function DreamWallet({cardArray}) {
         'Content-type': 'application/json; charset=UTF-8'
         },
     })
-    let cardItemIndex = showArray.indexOf(card)
+    let cardItemIndex = cardArray.indexOf(card)
     if(cardItemIndex > -1) {
-      let newArray = [...showArray]
+      let newArray = [...cardArray]
       newArray.splice(cardItemIndex, 1)
-      setShowArray(newArray)
+      setCardArray(newArray)
     }
     }
 
@@ -101,15 +104,21 @@ function DreamWallet({cardArray}) {
     <>
       <div id='dreamWallet'> 
         <div id="Form">
+          <div id='dreamInputs'>
             <h2 id="DreamWalletHeading">Dream Wallet</h2>
-            <h4 id="DreamWalletHeading1"> Anything is Possible  </h4>
-            <h4 id="actualInfo">Here's where you can visualize the crypto that you're currently Hodling. See where your investments currently stand or envision a path for future saving.</h4>
-            <div id='dreamInputs'>
-            <input  type="text" id="userWalletInput1" name="fname" placeholder="Enter Your Dream Amount of BTC" onChange={handleDreamBTCBase}></input>
-            <input id='userWalletInput1' placeholder='Set Your Dream BTC Price' onChange={handleDreamBTCMultiplication}></input>
-            <input type="text" id="userWalletInput2" name="lname" placeholder="Enter Your Dream Amount of ETH" onChange={handleEthBase}></input>
-            <input id='userWalletInput2' placeholder='Set Your Dream ETH Price' onChange={handleEthMultiplication}></input>
+            <h4 id="Dreams"> Anything is Possible  </h4>
+          
+
+            <label id="actualWalletLabel"> Your BTC </label>
+            <input  type="text" id="userWalletInput" name="fname" placeholder="Enter BTC" onChange={handleDreamBTCBase}></input>
+            <label id='DreamBTCLabel'>Dream Price </label>
+            <input id='DreamBTCInput' placeholder='set the price..' onChange={handleDreamBTCMultiplication}></input>
+            <label id="actualWalletLabel">Your ETH </label>
+            <input type="text" id="userWalletInput" name="lname" placeholder="Enter ETH" onChange={handleEthBase}></input>
+            <label id='DreamBTCLabel'>Dream Price </label>
+            <input id='DreamBTCInput' placeholder='set the price..' onChange={handleEthMultiplication}></input>
           </div>
+        </div> 
 
         <div id="dreamDollarValueChart">
             <h3>Dollar Value</h3>
@@ -123,11 +132,9 @@ function DreamWallet({cardArray}) {
               <button className='btn btn-primary' onClick={handleAddToJson}>Create Card</button>
         </div>
         <div>
-          {newArray}
           {mappedArray}
         </div>
       </div>
-      </div> 
     </>
   )
 }
