fedb7e7ba0809c60be6ee07e72338b298edcb75d
Alex Manzi <alex.manzi22@gmail.com>
2022-03-11 11:38:04 -0500

    navbar

--- src/CryptoCard.js
+++ src/CryptoCard.js
@@ -1,6 +1,14 @@
 import React from 'react'
 
 function CryptoCard({card, handleDelete}) {
+  let btcTotal = card.BTCTotal
+  let ethTotal = card.EthTotal
+  let grandTotal = card.total
+
+  let formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  })
 
   return (
     <>
@@ -8,9 +16,9 @@ function CryptoCard({card, handleDelete}) {
   <div className="card-body">
     <h5 className="card-title">{card.name}</h5>
     <br></br>
-    <p>{card.DreamBTCAmount} BTC =${card.BTCTotal}</p>
-    <p> {card.DreamEthAmount} ETH =${card.EthTotal}</p>
-    <h5 id='cardTotal'>Total = {card.total}</h5>
+    <p>{card.DreamBTCAmount} BTC = {formatter.format(btcTotal)}</p>
+    <p> {card.DreamEthAmount} ETH = {formatter.format(ethTotal)}</p>
+    <h5 id='cardTotal'>Total = {formatter.format(grandTotal)}</h5>
     <a id='deletebttn' onClick={() => handleDelete(card)}className="btn btn-danger btn-sm rounded-0">X</a>
   </div>
 </div>
