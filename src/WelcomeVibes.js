6856be60baa446c27c8fb7941279fdb4f8decff4
Alex Manzi <alex.manzi22@gmail.com>
2022-03-11 12:35:54 -0500

    close to finished

--- src/WelcomeVibes.js
+++ src/WelcomeVibes.js
@@ -10,21 +10,23 @@ function WelcomeVibes({data}) {
   return (
     <div id='container'>
     <h1 className="Cryptonaire">CRYPTONAIRE</h1>
-    <h5 className="dreamBig">Dream Bigger</h5>
     
 
     <div className='leftTitle'>
         <br></br>
-        <h3>REIMAGINE YOUR CRYPTO EXPERIENCE</h3>
+        <h3 id='imagine'>REIMAGINE YOUR CRYPTO</h3>
         <br></br>
         <br></br>
-        <h3 id=''>  </h3>
+      
         <Link to="/actualWallet"><button type="button" id='largeWelcomeBttn' className="btn btn-primary btn-lg">Reality</button> </Link>
        <Link to='/dreamWallet'> <button type="button" id='largeWelcomeBttn2'  className="btn btn-secondary btn-lg">Dream</button> </Link>
     <p id='beFearless'>BE FEARLESS</p>
         
 
     </div>
+
+    <h5 className="dreamBig">Dream Bigger</h5>
+
 
 
 
